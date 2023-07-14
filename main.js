ce58945b5dbd7d56346073cb39c86b4bc080e8a8
lionezajoao <joaopedrobzlioneza@hotmail.com>
2023-07-14 00:07:38 -0300

    Backup to sarraialcool

--- main.js
+++ main.js
@@ -8,14 +8,20 @@ const main = async () => {
 *!teste* -> Outra mensagem fofa
 *!duvido* -> Uma ajuda fofa
 *!vampeta* -> Uma foto fofa
+*!vampetasso* -> Uma foto mais fofa
 *!gatinho* -> Um gatinho fofo
+*!vasco* -> Um time fofo
     `)
 
     Messages.handleCommand("!querida", "QUERIDA É MINHA XERECA");
     Messages.handleCommand("!teste", "VAI TOMAR NO CU PIRANHA");
     Messages.handleCommand("!duvido", "MEU PAU NO SEU OUVIDO");
     Messages.sendMediaMessage("!vampeta", "./media/vampeta.jpg");
+    Messages.sendMediaMessage("!vampetasso", "./media/vampetasso.jpeg");
     Messages.sendMediaMessage("!gatinho", "./media/miau.mp3");
+    // Messages.sendVideoMessage("!lula", "./media/lula.mpeg");
+    Messages.sendMediaMessage("!vasco", "./media/vascao.jpeg");
+    Messages.loveMail();
 }
 
 main()
--- messages.js
+++ messages.js
@@ -39,18 +39,54 @@ export default class Messages {
     static handleCommand(command, response) {
         this.client.on('message', message => {
             if(message.body === command) {
+                console.log(`Command ${ command } called from ${ message.from }`);
                 message.reply(response);
             }
         });
     }
 
-    static sendMediaMessage(command, media) {
-        const newMedia = wpp.MessageMedia.fromFilePath(media)
+    static sendMediaMessage(command, mediaPath) {
+        const newMedia = wpp.MessageMedia.fromFilePath(mediaPath)
         this.client.on('message', message => {
             if(message.body === command) {
+                console.log(`Command ${ command } called from ${ message.from }`);
                 message.reply(newMedia);
             }
         });
+    }
+
+    static sendVideoMessage(command, mediaPath) {
+        const newMedia = wpp.MessageMedia.fromFilePath(mediaPath);
+        this.client.on("message", message => {
+            if(message.body === command) {
+                this.client.sendMessage(message.from, newMedia);
+            }
+        });
+    }
+
+    static loveMail() {
+        const responses = [
+            "Ta precisando fuder hein...\nTomara que consiga!",
+            "Que esse curreio te dê sorte!\nCurta o Sarraiálcool!",
+        ]
+
+        const meanResponses = [
+            "Você precisa mandar alguma mensagem né coisa burra",
+            "Sabe chegar em ninguém não, arrombade? Tu esqueceu da mensagem!"
+        ]
+        this.client.on('message', message => {
+            console.log(`Command !curreio called from ${ message.from }`);
+            if(message.body.startsWith("!curreio")) {
+                const reply = message.body.split("!curreio");
+                if(reply[1] == "") {
+                    message.reply(meanResponses[Math.floor(Math.random()*responses.length)]);
+                } else {
+                    message.reply(`${ responses[Math.floor(Math.random()*responses.length)] }\n\nAgora, beba essa quantidade de shots: ${ Math.floor(Math.random()*5 + 1) }`);
+                    this.client.sendMessage("120363143055632545@g.us", `*NOVO CORREIO!*\n\n${ reply[1] }`);
+                }
+                
+            }
+        })
     }
 
 }
